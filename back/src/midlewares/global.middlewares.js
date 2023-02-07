bf8a118d974f9f5b6d86c5b9eaf42fa6a994b81f
julio <juliorocha207@gmail.com>
2023-02-07 11:39:37 -0400

    att middleware

---
+++ back/src/midlewares/global.middlewares.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const UserService = require("../services/user.service");
+
+const validid = (req, res , next) =>{
+
+        const
+}
+
+const validUser = (req, res, next) =>{
+
+}
+
+module.exports = {validUser, validid}
