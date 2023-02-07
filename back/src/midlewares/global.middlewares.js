6e5a520b853cd789949cd7cc3498debaad5f9311
julio <juliorocha207@gmail.com>
2023-02-07 12:33:17 -0400

    att*

--- back/src/midlewares/global.middlewares.js
+++ back/src/midlewares/global.middlewares.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
 const UserService = require("../services/user.service");
 
-const validid = (req, res , next) =>{
+const validId = (req, res , next) =>{
+    const id = req.params.id
 
-        const
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "user not found" });
+    }
+    
 }
 
-const validUser = (req, res, next) =>{
+const validUser = async (req, res, next) =>{
 
+    const id = req.params.id
+
+    const user = await UserService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: "User not Found" });
+    }
 }
 
-module.exports = {validUser, validid}
+module.exports = {validUser, validId}
