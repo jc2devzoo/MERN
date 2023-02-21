a43b6603b02d7f9b023915d3c869142f6bbc2840
julio <juliorocha207@gmail.com>
2023-02-21 08:35:36 -0400

    "news" model add

---
+++ back/src/models/News..js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const NewsSchema = new mongooseSchema({
+    title:{
+        type: String,
+        require:true
+    },
+
+    text:{
+        type: String,
+        require: true
+    },
+
+    banner:{
+        type: String,
+        require: true
+    },
+
+    createdAt:{
+        type:Date,
+        default: Date.now(),
+    },
+
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        require: true
+    },
+
+    likes:{
+        type: Array,
+        require: true
+    },
+
+    comments:{
+    type: Array,
+    require: true
+    }
+});
+
+const News = mongoose.model("News", NewsSchema);
+
+export default News;
